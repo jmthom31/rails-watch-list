6805d512a5d7b6eda44e075157db8dda8e15019f
Julia Thomas <juliathomas@outlook.com>
2021-11-11 15:42:16 +0000

    Added controllers and views for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/show'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :lists, only: [:show, :index, :new, :create] do
+    member do
+      get :bookmarks, only: [:new, :create, :destroy]
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Movie.destroy_all
+
+puts 'Creating movies...'
+15.times do
+  movie = Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: ['https://unsplash.com/photos/CiUR8zISX60', 'https://unsplash.com/photos/evlkOfkQ5rE', 'https://unsplash.com/photos/MAYsdoYpGuk', 'https://unsplash.com/photos/q8P8YoR6erg'].sample,
+    rating: [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0].sample
+  )
+  puts "Creating movie with id #{movie.id}"
+end
+
+puts 'Fin!'
